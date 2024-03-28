5535449144e2a38cc9a285ad2f3ba66b1846a65b
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-03-28 17:28:56 -0300

    Creating the Compromissos Class

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Compromissos(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    event_date = models.DateTimeField()
+    creation_date = models.DateTimeField(auto_now=True)
