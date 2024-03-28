705bda9dc05f755989db0dadc5fb452bc62a89f7
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-03-28 17:41:46 -0300

    Creating the model admin

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from agenda.models import Compromissos
 
-# Register your models here.
+
+class CompromissoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'event_date',)
+    search_fields = ('name', 'event_date',)
+
+
+admin.site.register(Compromissos, CompromissoAdmin)
+
