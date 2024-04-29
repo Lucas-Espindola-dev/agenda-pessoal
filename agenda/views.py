d73caef4fa30ae914bb375e88c2f1bf0310311c0
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-04-29 10:30:36 -0300

    Adding the serializer class

--- agenda/views.py
+++ agenda/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
 from agenda.models import Compromissos
+from agenda.serializer import CompromissoSerializer
 
 
 class AgendaViewset(viewsets.ModelViewSet):
     queryset = Compromissos.objects.all()
-    serializer_class = None
+    serializer_class = CompromissoSerializer
